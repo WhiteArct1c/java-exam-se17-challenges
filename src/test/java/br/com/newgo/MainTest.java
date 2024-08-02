4f2bd70e5ef4bf7a7737130f5d198921d955a56a
WhiteArct1c <matrodrigues1576@gmail.com>
2024-08-02 16:02:53 -0300

    chore: add junit5 dependency

---
+++ src/test/java/br/com/newgo/MainTest.java
@@ -0,0 +1,14 @@
+package br.com.newgo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void test(){
+        int numberTest = 2;
+        assertEquals(2, numberTest);
+    }
+}
