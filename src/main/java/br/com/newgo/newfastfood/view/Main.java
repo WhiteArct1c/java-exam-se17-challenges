0011953a5f5ca4bed76d3bd7e741e66a3bff5cb6
WhiteArct1c <matrodrigues1576@gmail.com>
2024-08-02 20:05:16 -0300

    test: add a simple test

--- src/main/java/br/com/newgo/newfastfood/view/Main.java
+++ src/main/java/br/com/newgo/newfastfood/view/Main.java
@@ -1,7 +1,11 @@
 package br.com.newgo.newfastfood.view;
 
+import br.com.newgo.newfastfood.model.impl.users.Customer;
+import br.com.newgo.newfastfood.repository.UserRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UserRepository userRepository = new UserRepository();
+        userRepository.addUser(new Customer(1L, "John Doe", "john@gmail.com", "123456789"));
     }
 }
