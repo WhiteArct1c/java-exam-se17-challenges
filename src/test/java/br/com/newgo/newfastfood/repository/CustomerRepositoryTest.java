545551d7d9ae8e4588d63cd7313b00a76bd9c7e3
WhiteArct1c <matrodrigues1576@gmail.com>
2024-08-05 10:44:26 -0300

    test: add tests for get customer age between a range

--- src/test/java/br/com/newgo/newfastfood/repository/CustomerRepositoryTest.java
+++ src/test/java/br/com/newgo/newfastfood/repository/CustomerRepositoryTest.java
@@ -13,17 +13,17 @@ public class CustomerRepositoryTest {
 
     @BeforeEach
     public void setUp() {
-        customerRepository.addUser(new Customer(1L, "John Doe", "john@gmail.com", "1199998888"));
-        customerRepository.addUser(new Customer(2L, "Jane Doe", "jane@gmail.com", "1187879999"));
-        customerRepository.addUser(new Customer(3L, "Alice", "alice@gmail.com", "1178099999"));
-        customerRepository.addUser(new Customer(4L, "Bob", "bob@gmail.com", "1166669999"));
-        customerRepository.addUser(new Customer(5L, "Charlie", "charlie@gmail.com", "1155559999"));
-        customerRepository.addUser(new Customer(6L, "David", "david@gmail.com", "1144449999"));
+        customerRepository.addUser(new Customer(1L, "John Doe", 24,"john@gmail.com", "1199998888"));
+        customerRepository.addUser(new Customer(2L, "Jane Doe", 33,"jane@gmail.com", "1187879999"));
+        customerRepository.addUser(new Customer(3L, "Alice", 32,"alice@gmail.com", "1178099999"));
+        customerRepository.addUser(new Customer(4L, "Bob", 40,"bob@gmail.com", "1166669999"));
+        customerRepository.addUser(new Customer(5L, "Charlie", 20,"charlie@gmail.com", "1155559999"));
+        customerRepository.addUser(new Customer(6L, "David", 18,"david@gmail.com", "1144449999"));
     }
 
     @Test
     public void testAddUser() {
-        customerRepository.addUser(new Customer(7L, "Eve", "eve@gmail.com", "1133339999"));
+        customerRepository.addUser(new Customer(7L, "Eve", 19,"eve@gmail.com", "1133339999"));
         assertEquals(7, customerRepository.getCustomers().size());
     }
 
@@ -53,6 +53,12 @@ public class CustomerRepositoryTest {
     public void testGetAllUsersSortedByName(){
         assertNotNull(customerRepository.getUsersSortedByName());
         assertEquals("Alice", customerRepository.getUsersSortedByName().get(0).getName());
+    }
+
+    @Test
+    public void testGetUsersBetweenAges(){
+        assertNotNull(customerRepository.getUsersBetweenAges(20, 30));
+        assertEquals(3, customerRepository.getUsersBetweenAges(20, 30).size());
     }
 
     @Test
